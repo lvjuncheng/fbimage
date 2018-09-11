4901b80f0432bd5c590ceb2614e5fbd92ceb5313
ljc <lvjuncheng@shining3d.com>
2018-09-12 05:49:58 +0800

    start

--- Sn3dFBImage/main.cpp
+++ Sn3dFBImage/main.cpp
@@ -1,6 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include "sn3DImageData.h"
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
